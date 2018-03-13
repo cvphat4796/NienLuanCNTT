bfb227b136d55cd6352e5a5437635d07794c0d14
Cao Vĩnh Phát <vinhphat471996@gmail.com>
2018-03-13 11:26:51 +0700

    conf

--- config/database.php
+++ config/database.php
@@ -56,11 +56,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'ec2-50-16-217-122.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'de2a8ioo85hria'),
+            'username' => env('DB_USERNAME', 'grfmubvwawjeou'),
+            'password' => env('DB_PASSWORD', '8ff0b08fa658a369b1bbe1fd97610a3205a379b0284a4fa4d7d57a400c617d2d'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
