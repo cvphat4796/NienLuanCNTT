f9a1b34b18cc2024701bbe1821a85539bc979a93
Cao Vĩnh Phát <vinhphat471996@gmail.com>
2018-03-20 16:19:48 +0700

    fix bug

--- app/Http/Controllers/BoGDController.php
+++ app/Http/Controllers/BoGDController.php
@@ -114,7 +114,7 @@ class BoGDController extends Controller
 	public function getListLoaiThoiGianBoGD()
 	{
 		$loaithoigian = DB::table('loaithoigian')->get();
-		
+		dd($loaithoigian);
 		 return Datatables::of($loaithoigian)->make(true);
 	}
 	// het action lay du lieu  thoi gian
